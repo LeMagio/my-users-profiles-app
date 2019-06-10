5596154e45b4be7040d4b65a6da00ff4bd135fb8
munozg2x <jose.luisx.munoz.gonzalez@intel.com>
2019-06-10 16:59:38 -0500

    Fix to the engines

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/users-profile-app'));
+app.use(express.static(__dirname + '/dist'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/users-profile-app/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // default Heroku PORT
